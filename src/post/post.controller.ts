2721b56e47139e0324b72c07a939fd2f3106b37a
Guy de Messimy <gdemessimy@gmail.com>
2022-06-23 14:36:24 +0200

    feat : post module

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,14 +1,81 @@
-import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { Post as PostModel } from '@prisma/client';
 import { Public } from 'src/common/decorators/public.decorator';
 import { CreatePostDto } from './dto/create-post.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 
 @ApiTags('posts')
 @Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+  @Public()
+  @Get('all')
+  async getPosts(@Query() params): Promise<PostModel[]> {
+    return this.postService.posts(params);
+  }
+
+  @Public()
+  @Get('filtered-post')
+  async getFilterPosts(
+    @Query() paginationQuery: PaginationQueryDto,
+  ): Promise<PostModel[]> {
+    const { limit, offset, orderBy, where } = paginationQuery;
+    return this.postService.posts({
+      limit,
+      offset,
+      orderBy: {
+        id: orderBy,
+      },
+      where: {
+        id: Number(where),
+      },
+    });
+  }
+
+  @Public()
+  @Get('published')
+  async getPublishedPosts(): Promise<PostModel[]> {
+    return this.postService.posts({
+      where: { published: true },
+    });
+  }
+
+  @Public()
+  @Get('filtered-posts/:searchString')
+  async getFilteredPosts(
+    @Param('searchString') searchString: string,
+  ): Promise<PostModel[]> {
+    return this.postService.posts({
+      where: {
+        OR: [
+          {
+            title: { contains: searchString },
+          },
+          {
+            content: { contains: searchString },
+          },
+        ],
+      },
+    });
+  }
+
+  @Public()
+  @Get('post/:id')
+  async getPostById(@Param('id') id: string): Promise<PostModel> {
+    return this.postService.post({ id: Number(id) });
+  }
 
   @Public()
   @Post('post')
@@ -25,9 +92,12 @@ export class PostController {
   }
 
   @Public()
-  @Get('post/:id')
-  async getPostById(@Param('id') id: string): Promise<PostModel> {
-    return this.postService.post({ id: Number(id) });
+  @Put('publish/:id')
+  async publishPost(@Param('id') id: string): Promise<PostModel> {
+    return this.postService.updatePost({
+      where: { id: Number(id) },
+      data: { published: true },
+    });
   }
 
   @Public()
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import { Post, Prisma } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
 import { UserService } from 'src/user/user.service';
+import { PostInput } from './entities/post.entity';
 
 @Injectable()
 export class PostService {
@@ -12,17 +13,39 @@ export class PostService {
     private readonly configService: ConfigService,
   ) {}
 
+  async post(
+    postWhereUniqueInput: Prisma.PostWhereUniqueInput,
+  ): Promise<Post | null> {
+    return this.prisma.post.findUnique({
+      where: postWhereUniqueInput,
+    });
+  }
+
+  async posts(paginationQuery: PostInput): Promise<Post[]> {
+    const { limit, offset, cursor, where, orderBy } = paginationQuery;
+    return this.prisma.post.findMany({
+      skip: offset,
+      take: limit,
+      orderBy,
+      cursor,
+      where,
+    });
+  }
+
   async createPost(data: Prisma.PostCreateInput): Promise<Post> {
     return this.prisma.post.create({
       data,
     });
   }
 
-  async post(
-    postWhereUniqueInput: Prisma.PostWhereUniqueInput,
-  ): Promise<Post | null> {
-    return this.prisma.post.findUnique({
-      where: postWhereUniqueInput,
+  async updatePost(params: {
+    where: Prisma.PostWhereUniqueInput;
+    data: Prisma.PostUpdateInput;
+  }): Promise<Post> {
+    const { data, where } = params;
+    return this.prisma.post.update({
+      data,
+      where,
     });
   }
 
